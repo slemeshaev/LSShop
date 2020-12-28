67b7b6a853abb22b56642a84930fd78747f047b3
slemeshaev <lemeshaevs@gmail.com>
2020-12-28 10:13:38 +0300

    Удалил лишнюю строчку

--- LSShop/LSShop/App/SceneDelegate.swift
+++ LSShop/LSShop/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let navigationController = UINavigationController(rootViewController: LoginController())
+        let navigationController = UINavigationController(rootViewController: ProductCatalogController())
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
